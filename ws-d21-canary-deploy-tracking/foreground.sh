651bb4a77445f470ea08eeb215d5ec490f3a5ceb
tammergalal <tammer.galal@datadoghq.com>
2021-10-27 11:56:45 -0400

    making some changes to ws

--- ws-d21-canary-deploy-tracking/foreground.sh
+++ ws-d21-canary-deploy-tracking/foreground.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-statuscheck "tools"
-
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
-
-[ ! -d "/root/lab" ] && mkdir /root/lab 
-
-cd /root/lab
-
-printf "DD_API_KEY=$DD_API_KEY\n\
-DD_APP_KEY=$DD_APP_KEY\n\
-POSTGRES_USER=$POSTGRES_USER\n\
-POSTGRES_PASSWORD=$POSTGRES_PASSWORD\n" > /root/lab/.env 
+while [ ! -f "/usr/local/bin/prepenvironment" ]; do
+  sleep 0.3
+done
+sleep 0.3
 
 clear
 
-statusupdate "environment"
-statuscheck "workspace"
-
-statusupdate complete
 prepenvironment
